0a1d384c88db12f6ee22b302426f2423d2a8ee4f
Philip Ford <pford68@earthlink.net>
2016-12-03 14:54:17 -0500

    Added angular2 and shopping cart

---
+++ src/ts/components/Cart/CartController.ts
@@ -0,0 +1,11 @@
+'use strict';
+import {Component} from "@angular/core";
+
+/*
+
+*/
+@Component({
+    selector: 'shopping-cart',
+    templateUrl: 'shopping-cart.component.html',
+    styleUrls: [ 'shopping-cart.component.css' ]
+})
---
+++ src/ts/components/Cart/index.ts
@@ -0,0 +1,15 @@
+/**
+ *
+ */
+'use strict';
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent }  from './app.component';
+
+@NgModule({
+    imports: [ BrowserModule ],
+    declarations: [ AppComponent ],
+    bootstrap: [ AppComponent ]
+})
+export class AppModule { }
